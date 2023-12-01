9144a755c122a0a0eb1abc1550a90270b9382ee4
R3FA <skopakfaris@hotmail.com>
2023-12-01 18:12:53 +0100

    Fix component naming

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
-import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { PagenotfoundComponent } from './components/page-not-found/pagenotfound.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/main-component', pathMatch: 'full' },
