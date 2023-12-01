5acdc35e652443c68e52a9622c5b4fc491bdc419
R3FA <skopakfaris@hotmail.com>
2023-12-01 18:07:51 +0100

    Set up initial routing and refactor app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/main-component', pathMatch: 'full' },
+  { path: 'main-component', component: MainComponent },
+  { path: '**', component: PagenotfoundComponent },
+];
