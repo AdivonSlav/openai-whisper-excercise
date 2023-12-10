4035a5d442bd31a15c5ed7e4b7ef8a2a12579c96
R3FA <skopakfaris@hotmail.com>
2023-12-10 21:48:54 +0100

    Add OpenAI API URL and API Authentication key

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
   whisperCppApiUrl: process.env['WHISPER_CPP_API_URL'],
+  openAIUrl: 'https://api.openai.com/v1/images/generations',
+  authorization: 'Bearer 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',
 };
